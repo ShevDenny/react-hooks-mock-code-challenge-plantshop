6ec5872bf3e836c78c32afa404ce5fc0ca888514
ShevDenny <sdenny904@gmail.com>
2021-06-16 13:08:31 -0400

    done

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 
-function PlantCard() {
+function PlantCard({plant, removePlant}) {
+  const {id, name, image ="https://via.placeholder.com/400", price} = plant
+  const [outOfStock, setOutOfStock] = useState(true)
+
+  function handleStock(){
+    setOutOfStock(outOfStock => !outOfStock)
+  }
+
+  function handleRemove() {
+    fetch(`http://localhost:6001/plants/${id}`, {
+  method: 'DELETE',
+  });
+  removePlant(id);
+  }
+
+  function updatePrice(){
+      fetch(`http://localhost:6001/plants/${id}`, {
+          method: 'PATCH',
+
+          headers: {
+          "Content-type": "application/json"
+          },         
+           body: JSON.stringify({price:price}),
+          })
+          .then(response => response.json())
+          .then(json => console.log(json))
+  }
+
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      
+        <button 
+          onClick={handleStock} 
+          className={outOfStock ? "primary" : ""}>{outOfStock ? "In Stock" : "Out of Stock"}
+        </button>
+        <button
+          onClick={handleRemove}>Remove Plant
+        </button>
+
+      
+      
     </li>
   );
 }
